084b73ac25f8e9384b4db53a4eed91884f30ba25
Leandro Scardua <leandroscar@gmail.com>
2020-12-14 16:17:48 -0500

    Update install_containerd.ps1

--- install_containerd.ps1
+++ install_containerd.ps1
@@ -1,18 +1,38 @@
-mkdir c:\containerd
-cd c:\containerd
-curl -OL https://github.com/containerd/containerd/releases/download/v1.4.0/containerd-1.4.0-windows-amd64.tar.gz
-tar xvf .\containerd-1.4.0-windows-amd64.tar.gz
+$ErrorActionPreference = 'Stop'
 
-# extract and configure
-Copy-Item -Path ".\bin\" -Destination "$Env:ProgramFiles\containerd" -Recurse -Force
-cd $Env:ProgramFiles\containerd\
+Write-Host "Checking the latest version of containerd"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$tag = (Invoke-WebRequest "https://api.github.com/repos/containerd/containerd/releases/latest" | ConvertFrom-Json)[0].tag_name
+
+$destination="$env:HOMEDRIVE\containerd"
+Write-Host "Creating folder on $destination"
+mkdir -force $destination | Out-Null
+cd $destination
+
+$dlw = $tag -replace "v",""
+Write-Host "Downloading containerd to $destination"
+Invoke-WebRequest "https://github.com/containerd/containerd/releases/download/$tag/containerd-$dlw-windows-amd64.tar.gz" -UseBasicParsing -OutFile $destination\containerd-$dlw-windows-amd64.tar.gz
+
+Write-Host "Saving containerd on $destination"
+
+tar.exe -xf .\containerd-$dlw-windows-amd64.tar.gz
+
+Copy-Item -Path "$destination\bin\*" -Destination $destination -Recurse -Force
+
+Write-Host "creating containerd config file"
+
 .\containerd.exe config default | Out-File config.toml -Encoding ascii
 
-# review the configuration. depending on setup you may want to adjust:
-# - the sandbox_image (kubernetes pause image)
-# - cni bin_dir and conf_dir locations
-Get-Content config.toml
+Write-Host "Registering containerd"
 
 .\containerd.exe --register-service
 
+Write-Host "starting containerd"
+
 Start-Service containerd
+
+Write-Host "Cleaning up containerd folder"
+
+Remove-Item containerd-$dlw-windows-amd64.tar.gz
+
+Remove-item -Path .\bin -Recurse
